a086f089569d8e7da52c17bab665a4fafca4cb36
JoacoR7 <joaquin.ruiz2012@gmail.com>
2026-03-21 21:22:56 -0300

    Ejercicio terminado

--- app/src/main/java/com/example/ejemplo01/MainActivity.java
+++ app/src/main/java/com/example/ejemplo01/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.ejemplo01;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +14,10 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Campos de texto
+    EditText nombre, apellido;
+    // Botón enviar
+    Button btnEnviar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +27,24 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Inicialización de componentes
+        btnEnviar = findViewById(R.id.btnEnviar);
+        nombre = findViewById(R.id.nombre);
+        apellido = findViewById(R.id.apellido);
+        
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(nombre.getText().toString().isEmpty()) {
+                    nombre.setError("Falta colocar el nombre");
+                } else if (apellido.getText().toString().isEmpty()) {
+                    apellido.setError("Falta colocar el apellido");
+                } else {
+                    Toast.makeText(getBaseContext(), "Almacenado", Toast.LENGTH_LONG).show();
+                }
+            }
         });
     }
 }
