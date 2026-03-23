d8c18a8f2ea50dddce175b073f7c0205d3425f42
Tomás Rando <tomasrando@gmail.com>
2026-03-23 15:57:48 -0300

    Add ex from video 2. Reorganize files

---
+++ Ejercicio1/EjercicioA/Video1/Tomas/app/src/main/java/com/tomas/ejemplo01/MainActivity.java
@@ -0,0 +1,52 @@
+package com.tomas.ejemplo01;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText nombre, apellido;
+    Button btnEnviar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        //Inicializamos
+        btnEnviar = findViewById(R.id.btnEnviar);
+        nombre = findViewById(R.id.textNombre);
+        apellido = findViewById(R.id.textApellido);
+
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (nombre.getText().toString().isEmpty()) {
+                    nombre.setError("Debe ingresar el nombre");
+                }
+                else {
+                    if (apellido.getText().toString().isEmpty()) {
+                        apellido.setError("Debe ingresar el apellido");
+                    }
+                    else {
+                        Toast.makeText(getBaseContext(), "Almacenado", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+    }
+}
