00da331f5910ae0005fb88d8f6e2799499117574
JoacoR7 <joaquin.ruiz2012@gmail.com>
2026-03-29 18:59:12 -0300

    Ej 2 - D: Avance primer video

---
+++ Ejercicio2/EjercicioD/Joaquin/app/src/main/java/com/example/ejerciciod/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.ejerciciod;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.ejerciciod.db.DbHelper;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnCrear;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        btnCrear = findViewById(R.id.btnCrear);
+
+        btnCrear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DbHelper dbHelper = new DbHelper(MainActivity.this);
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                if(db != null) {
+                    Toast.makeText(MainActivity.this, "Base de datos creada exitosamente", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Error al crear la base de datos", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
